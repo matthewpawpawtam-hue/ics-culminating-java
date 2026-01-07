863da9768889e49e10a4a59bbd4cb345d3f72925
Matt <MatthewPawPawTam@gmail.com>
2026-01-07 17:45:45 -0500

    Good progress I think

--- culminating/src/mainSnakeGame.java
+++ culminating/src/mainSnakeGame.java
@@ -1,19 +1,30 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
+import javax.swing.*;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 @SuppressWarnings("serial")
-public class mainSnakeGame extends JPanel implements Runnable, KeyListener {
+public class mainSnakeGame extends JPanel implements Runnable, KeyListener, ActionListener {
 	
-	Rectangle rect = new Rectangle(0, 0, 30, 30);
 	Rectangle[] walls = new Rectangle[5];
 	boolean up, down, left, right;
-	int speed = 4;
+	int speed = 6;
 	int screenWidth = 680;
 	int screenHeight = 680;
 	Thread thread;
 	int FPS = 60;
-    Graphics offScreenBuffer;
+    Graphics offScreenBuffer;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
     Image offScreenImage;
 	int SQUARE_SIZE = 60;
 	int board[][];
@@ -21,18 +32,25 @@ public class mainSnakeGame extends JPanel implements Runnable, KeyListener {
 	int xPos, yPos;
 	int randX, randY;
 	int [][] snake = new int [100][2];
+	int snakeLength = 3;
+	int points = 0;
+	long startTime, timeElapsed;
+	int frameCount = 0;
 	
 	public mainSnakeGame() {
+		setFocusable(true);
+		requestFocusInWindow();
+		addKeyListener(this);
 		setPreferredSize(new Dimension(screenWidth, screenHeight));
 		setVisible(true);
         board = new int [10][10];
 		thread = new Thread(this);
 		thread.start();
-		snake[0][0] = 2;
+		snake[0][0] = 0;
 		snake[0][1] = 4;
 		snake[1][0] = 1;
 		snake[1][1] = 4;
-		snake[2][0] = 0;
+		snake[2][0] = 2;
 		snake[2][1] = 4;
 
 	}
@@ -53,44 +71,49 @@ public class mainSnakeGame extends JPanel implements Runnable, KeyListener {
 	}
 	
 	public void initialize() {
+		startTime = System.currentTimeMillis();
+		timeElapsed = 0;
+		FPS = 60;
+		for(int i = 0; i < 100000; i++) {
+			// this is just to delay time
+			String s = "set up stuff blah blah blah";
+			s.toUpperCase();
+		}
 
-		// Set apple image
+		// Set apple image in random location
 		apple = new ImageIcon(getClass().getResource("APPLE.png")).getImage();
-		randX = ((int)(Math.random()*10)) * SQUARE_SIZE + 40;
-		randY = ((int)(Math.random()*10)) * SQUARE_SIZE + 40;
+		randX = 465;
+		randY = 285;
 
-		// //setups before the game starts running
-		// walls[0] = new Rectangle(200, 200, 60, 60);
-		// walls[1] = new Rectangle(300, 40, 40, 100);
-		// walls[2] = new Rectangle(450, 100, 80, 35);
-		// walls[3] = new Rectangle(60, 60, 15, 15);
-		// walls[4] = new Rectangle(250, 350, 150, 200);
+		System.out.println("Thread: Done initializing game");
 	}
 	
 	public void update() {
+		//update stuff
+		timeElapsed = System.currentTimeMillis() - startTime;
+
+		frameCount++;
 		move();
 		keepInBound();
-		for(int i = 0; i < walls.length; i++){
-			checkCollision(walls[i]);
-		}
-
-		// check for collision with apple
-		// add to score + toggle growing snake
+		checkCollision();
 	}
 	
 	public void paintComponent(Graphics g) {
-		
-        // Set up the offscreen buffer the first time paint() is called
-        if (offScreenBuffer == null)
-		{
-			offScreenImage = createImage (this.getWidth (), this.getHeight ());
-			offScreenBuffer = offScreenImage.getGraphics ();
-		}
 		super.paintComponent(g);
+	// offScreenBuffer
+	if (offScreenImage == null ||
+	    offScreenImage.getWidth(null) != getWidth() ||
+    	offScreenImage.getHeight(null) != getHeight()) 
+	{
+    	offScreenImage = createImage(getWidth(), getHeight());
+    	offScreenBuffer = offScreenImage.getGraphics();
+	}
+
+		
 		offScreenBuffer.setColor(getBackground());
 		offScreenBuffer.fillRect(0, 0, getWidth(), getHeight());
-
 		Graphics2D g2 = (Graphics2D) g;
+
 		for (int row = 1; row < 11; row++) { // row
             for (int column = 1; column < 11; column++) { // column
 				
@@ -122,15 +145,9 @@ public class mainSnakeGame extends JPanel implements Runnable, KeyListener {
 				    	offScreenBuffer.fillRect (xPos, yPos, SQUARE_SIZE, SQUARE_SIZE);
                     }
                 }
-				
-				// Draw snake
-				offScreenBuffer.setColor(Color.BLUE);
-				offScreenBuffer.fillRect(40, 280, 180, 60);
-
-            	//g.drawImage(offScreenImage, 0, 0, null);
 			}
 		}
-		// draw apple
+		// Draw apple
 		offScreenBuffer.drawImage(apple, randX, randY, this);
 
         // Draw border
@@ -140,6 +157,20 @@ public class mainSnakeGame extends JPanel implements Runnable, KeyListener {
         offScreenBuffer.fillRect (0, 0, 40, 680);
         offScreenBuffer.fillRect (0, 640, 680, 40);
         offScreenBuffer.fillRect (640, 0, 44, 680);
+
+		// Draw snake
+		offScreenBuffer.setColor(Color.BLUE);
+		for (int i = 0; i < snakeLength; i++) {
+			offScreenBuffer.fillRect(40 + snake[i][0] * 60, 40 + snake[i][1] * 60, 60, 60);
+		}
+
+		// Display information stuff
+		offScreenBuffer.setColor(Color.WHITE);
+		offScreenBuffer.drawString("" + timeElapsed + " ms since start of program", 40, 18);
+		offScreenBuffer.drawString("" + timeElapsed/1000 + " seconds since the start of program", 40, 31);
+		offScreenBuffer.drawString(frameCount + " frames ran since the start of program", 280, 18);
+		offScreenBuffer.drawString("FPS: " + String.format("%.2f", (double)frameCount / ((double)timeElapsed / 1000)), 280, 31);
+		offScreenBuffer.drawString("Points: " + points, 580, 18);
 
         // Transfer the offScreenBuffer to the screen
 		g.drawImage (offScreenImage, 0, 0, this);
@@ -157,100 +188,112 @@ public class mainSnakeGame extends JPanel implements Runnable, KeyListener {
 		if(key == KeyEvent.VK_LEFT) {
 			left = true;
 			right = false;
+			up = false;
+			down = false;
 		}else if(key == KeyEvent.VK_RIGHT) {
-			right = true;
 			left = false;
+			right = true;
+			up = false;
+			down = false;
 		}else if(key == KeyEvent.VK_UP) {
+			left = false;
+			right = false;
 			up = true;
 			down = false;
 		}else if(key == KeyEvent.VK_DOWN) {
+			left = false;
+			right = false;
 			down = true;
 			up = false;
 		}
 	}
 
+	// public void snakeCoordinate {
+		
+	// }
+	
+	// void move() {
+	// 	if(left)
+	// 		rect.x -= speed;
+	// 	else if(right)
+	// 		rect.x += speed;
+	// 	if(up)
+	// 		rect.y += -speed;
+	// 	else if(down)
+	// 		rect.y += speed;
+	// }
+	
+	// void keepInBound() {
+	// 	if (rect.x < 40)
+	// 		rect.x = 40;
+	// 	else if (rect.x > 600)
+	// 		rect.x = 600;
+
+	// 	if (rect.y < 40)
+	// 		rect.y = 40;
+	// 	else if (rect.y > 600)
+	// 		rect.y = 600;
+	// }
+	
+	void spawnNewApple() {
+    	randX = ((int)(Math.random()*10)) * SQUARE_SIZE + 40;
+    	randY = ((int)(Math.random()*10)) * SQUARE_SIZE + 40;
+	}
+
+	// void checkCollision() {
+	// 	Rectangle appleRect = new Rectangle (randX, randY, apple.getWidth(null), apple.getHeight(null));
+	// 	if (rect.intersects(appleRect)){
+	// 		points ++;
+	// 		spawnNewApple();	
+	// 	}	
+	// }
+
 	@Override
 	public void keyReleased(KeyEvent e) {
-		int key = e.getKeyCode();
-		if(key == KeyEvent.VK_LEFT) {
-			left = false;
-		}else if(key == KeyEvent.VK_RIGHT) {
-			right = false;
-		}else if(key == KeyEvent.VK_UP) {
-			up = false;
-		}else if(key == KeyEvent.VK_DOWN) {
-			down = false;
-		}
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
 	}
-	
-	void move() {
 
-	}
-	
-	void keepInBound() {
-		if(rect.x < 0)
-			rect.x = 0;
-		else if(rect.x > screenWidth - rect.width)
-			rect.x = screenWidth - rect.width;
-		
-		if(rect.y < 0)
-			rect.y = 0;
-		else if(rect.y > screenHeight - rect.height)
-			rect.y = screenHeight - rect.height;
-	}
-	
-	void checkCollision(Rectangle wall) {
-		//check if rect touches wall
-		if(rect.intersects(wall)) {
-			System.out.println("collision");
-			//stop the rect from moving
-			double left1 = rect.getX();
-			double right1 = rect.getX() + rect.getWidth();
-			double top1 = rect.getY();
-			double bottom1 = rect.getY() + rect.getHeight();
-			double left2 = wall.getX();
-			double right2 = wall.getX() + wall.getWidth();
-			double top2 = wall.getY();
-			double bottom2 = wall.getY() + wall.getHeight();
-			
-			if(right1 > left2 && 
-			   left1 < left2 && 
-			   right1 - left2 < bottom1 - top2 && 
-			   right1 - left2 < bottom2 - top1)
-	        {
-	            //rect collides from left side of the wall
-				rect.x = wall.x - rect.width;
-	        }
-	        else if(left1 < right2 &&
-	        		right1 > right2 && 
-	        		right2 - left1 < bottom1 - top2 && 
-	        		right2 - left1 < bottom2 - top1)
-	        {
-	            //rect collides from right side of the wall
-	        	rect.x = wall.x + wall.width;
-	        }
-	        else if(bottom1 > top2 && top1 < top2)
-	        {
-	            //rect collides from top side of the wall
-	        	rect.y = wall.y - rect.height;
-	        }
-	        else if(top1 < bottom2 && bottom1 > bottom2)
-	        {
-	            //rect collides from bottom side of the wall
-	        	rect.y = wall.y + wall.height;
-	        }
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		String eventName = event.getActionCommand ();
+		if (eventName.equals("Exit")){
+			System.exit(0);
 		}
 	}
 	
 	public static void main(String[] args) {
-		JFrame frame = new JFrame ("Example");
+		// Set up the Game Menu
+		JMenu gameMenu = new JMenu ("Game");
+		
+		// Set up the Game MenuItems and add to gameMenu (with a separator)
+		JMenuItem newOption = new JMenuItem ("New");
+		JMenuItem exitOption = new JMenuItem ("Exit");
+
+		gameMenu.add (newOption);
+		gameMenu.addSeparator ();
+		gameMenu.add (exitOption);
+
+		JFrame frame = new JFrame ("P4 ICS3U Semester 1 - Snake Game Culminating (2025-2026) by Eric Lu & Matthew Tam");
 		mainSnakeGame myPanel = new mainSnakeGame();
+
+		newOption.setActionCommand ("New");
+		newOption.addActionListener (myPanel);
+		exitOption.setActionCommand ("Exit");
+		exitOption.addActionListener (myPanel);
+
+		// Add final menus to JMenuBar mainMenu
+		JMenuBar mainMenu = new JMenuBar ();
+		mainMenu.add (gameMenu);
+		
+		// Set the menu bar for this frame to mainMenu
+		frame.setJMenuBar (mainMenu);
 		frame.add(myPanel);
-		frame.addKeyListener(myPanel);
 		frame.setVisible(true);
+		myPanel.requestFocusInWindow();
 		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 	}
-}
+} // class
